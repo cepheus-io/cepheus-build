44b6a8576c1af953408f6644fa6d6f5d3c16ba50
Chris Jones <cj@hanscj.com>
2017-04-22 10:56:22 -0400

    Added in cepheus check

--- cepheus-build.sh
+++ cepheus-build.sh
@@ -72,6 +72,10 @@ echo -e "${RED}====> Bootstrap Initial Cepheus...${NC}"
 ./CEPH_UP -b
 
 echo -e "${YELLOW}====> Update Cepheus (-cepheus-build-)...${NC}"
+if [[ ! -d $HOME/cepheus ]]; then
+    mkdir -p $HOME/cepheus
+    cp -rp /tmp/cepheus/* $HOME/cepheus
+fi
 cd $HOME/cepheus
 # -x suppress logo, -u 0 update
 ./CEPH_UP -x -u 0
